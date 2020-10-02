f85ce7548468b0107814e4aaabd08ff91f1f8ef0
dil4devops <67610864+dil4devops@users.noreply.github.com>
2020-10-02 08:17:30 -0500

    Create apply_config.sh

---
+++ apply_config.sh
@@ -0,0 +1,7 @@
+kubectl apply -f iz-kafka-cp-kafka-connect-jmx-configmap
+kubectl apply -f iz-kafka-cp-kafka-jmx-configmap
+kubectl apply -f iz-kafka-cp-kafka-rest-jmx-configmap
+kubectl apply -f iz-kafka-cp-ksql-server-jmx-configmap
+kubectl apply -f iz-kafka-cp-ksql-server-ksql-queries-configmap
+kubectl apply -f iz-kafka-cp-schema-registry-jmx-configmap
+kubectl apply -f iz-kafka-cp-zookeeper-jmx-configmap
